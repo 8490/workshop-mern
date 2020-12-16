9401f7c0e7795c171408af29bc551cefeb7d9dbb
Mert <mert.san3407@gmail.com>
2020-12-16 19:52:04 +0300

    updated category router and controller

--- routers/CategoryRouter.js
+++ routers/CategoryRouter.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const CategoryController = require("../controllers/CategoryController");
-const validations = require("../middleware/validationMiddleware");
+const CategoryController = require('../controllers/CategoryController');
+const validations = require('../middleware/validationMiddleware');
 
 
-router.post("/addCategory", validations.categoryValidation, CategoryController.addCategory);
+router.post('/addCategory', validations.categoryValidation, CategoryController.addCategory);
 
-router.get("/getCategory/:id", CategoryController.getCategory);
+router.get('/getCategory/:id', CategoryController.getCategory);
 
-router.post("/updateCategory", validations.categoryValidation, CategoryController.updateCategory);
+router.post('/updateCategory', validations.categoryValidation, CategoryController.updateCategory);
 
-router.post("/deleteCategory/:id", CategoryController.deleteCategory);
+router.get('/deleteCategory/:id', CategoryController.deleteCategory);
 
-router.get("/", CategoryController.getCategories);
+router.get('/destroyCategory/:id', CategoryController.destroyCategory);
+
+router.get('/', CategoryController.getCategories);
 
 
 module.exports = router;
